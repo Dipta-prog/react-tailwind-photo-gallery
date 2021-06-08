016af7729cca6becb6f2f809e8279e7467757532
Dipta Sikder <anotheramit8@gmail.com>
2021-06-09 03:59:57 +0600

    some minor fix

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,22 +4,22 @@ const ImageCard = ({ image }) => {
   const tags = image.tags.split(',');
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-black">
       <img src={image.webformatURL} alt="" className="w-full"/>
       <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
+        <div className="font-bold text-blue-500 text-xl mb-2">
           Photo by {image.user}
         </div>
         <ul>
-          <li>
+          <li className="text-white">
             <strong>Views: </strong>
             {image.views}
           </li>
-          <li>
+          <li className="text-white">
             <strong>Downloads: </strong>
             {image.downloads}
           </li>
-          <li>
+          <li className="text-white">
             <strong>Likes: </strong>
             {image.likes}
           </li>
@@ -27,7 +27,7 @@ const ImageCard = ({ image }) => {
       </div>
       <div className="px-6 py-4">
         {tags.map((tag, index) => (
-          <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          <span key={index} className="inline-block bg-indigo-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
           #{tag}
         </span>
         ))}
